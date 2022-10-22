ea114254e82562726009e24643ad5b42d4a06d92
Geraldo Ramos <geraldorneto@gmail.com>
2022-10-22 19:47:34 +0000

    v1

--- maestro_worker_python/init.py
+++ maestro_worker_python/init.py
@@ -3,8 +3,6 @@ import argparse
 import os
 import pathlib
 
-dir = pathlib.Path(__file__).parent.resolve()
-
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -13,14 +11,14 @@ def main():
 
     args = parser.parse_args().__dict__
 
-    files = os.listdir(f"{dir}/worker-example/")
-    print(files)
+    dir = pathlib.Path(__file__).parent.resolve()
+    files = os.listdir(f"{dir}/worker_example")
     for file in files:
         if file == "models":
             continue
-        shutil.copy(f"{dir}/worker-example/{file}", args.get("folder"))
+        shutil.copy(f"{dir}/worker_example/{file}", args.get("folder"))
     
     os.mkdir(f"{args.get('folder')}/models")
-    shutil.copy(f"{dir}/worker-example/models/model.th", f"{args.get('folder')}/models/model.th")
+    shutil.copy(f"{dir}/worker_example/models/model.th", f"{args.get('folder')}/models/model.th")
 
 

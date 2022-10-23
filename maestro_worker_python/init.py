620492fd656a53ab2d34a96a27e190b6242aa4ea
Geraldo Ramos <geraldorneto@gmail.com>
2022-10-23 03:25:55 +0000

    init fix

--- maestro_worker_python/init.py
+++ maestro_worker_python/init.py
@@ -13,15 +13,13 @@ def main():
     args = parser.parse_args().__dict__
 
     dir = pathlib.Path(__file__).parent.resolve()
-    files = os.listdir(f"{dir}/data")
-    for file in files:
-        if file == "models":
-            continue
-        if file =="__pycache__":
-            continue
-        shutil.copy(f"{dir}/data/{file}", args.get("folder"))
-    
+    shutil.copy(f"{dir}/data/worker.py", f"{args.get('folder')}/worker.py")
+    shutil.copy(f"{dir}/data/requirements.txt", f"{args.get('folder')}/requirements.txt")
+    shutil.copy(f"{dir}/data/docker-compose.yaml", f"{args.get('folder')}/docker-compose.yaml")
+    shutil.copy(f"{dir}/data/Dockerfile", f"{args.get('folder')}/Dockerfile")
     os.mkdir(f"{args.get('folder')}/models")
     shutil.copy(f"{dir}/data/models/model.th", f"{args.get('folder')}/models/model.th")
+    
+
 
 

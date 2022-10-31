dd4a970c0b7f28e3f9044810cb71b50a39e49a68
Fernando Grunevald <fernandogrd@gmail.com>
2022-10-31 15:17:09 -0600

    remove BASE_PATH

--- maestro_worker_python/serve.py
+++ maestro_worker_python/serve.py
@@ -6,25 +6,24 @@ from starlette.concurrency import run_in_threadpool
 from importlib.machinery import SourceFileLoader
 
 
-BASE_PATH = os.environ.get("BASE_PATH", "/")
 MODEL_PATH = os.environ.get("MODEL_PATH", "./worker.py")
 
 worker = SourceFileLoader("worker",MODEL_PATH).load_module()
 model = worker.MoisesWorker()
 app = FastAPI()
 
-@app.post(f"{BASE_PATH}/inference")
+@app.post("/inference")
 async def inference(request: Request, response: Response):
     params = await request.json()
     return await run_in_threadpool(model.inference, input_data=params)
 
-@app.get(f"{BASE_PATH}/")
+@app.get("/")
 async def index(request: Request):
     return {"hostname": socket.gethostname(), "ok": True, "date": datetime.datetime.now()}
 
-@app.get(f"{BASE_PATH}/health")
+@app.get("/health")
 async def health(request: Request):
-    return {"ok": True}  
+    return {"ok": True}
 
 @app.on_event("shutdown")
 def shutdown_event():
--- maestro_worker_python/server.py
+++ maestro_worker_python/server.py
@@ -7,15 +7,12 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--worker", default="./worker.py",
                         help="Path to the Moises worker file that include a MoisesWorker class")
-    parser.add_argument("--base_path", default="/",
-                        help="Base path for the API, example: /your-worker")
     parser.add_argument("--port", default=8000, help="Port to run uvicorn on")
     parser.add_argument("--reload", default=False,
                         help="Reload the server on code changes")
-                        
+
     args = parser.parse_args().__dict__
     print(f"Running maestro server with {str(args)}")
     os.environ["MODEL_PATH"] = args.get("worker")
-    os.environ["BASE_PATH"] = args.get("base_path")
     uvicorn.run("maestro_worker_python.serve:app", host='0.0.0.0', port=int(args.get(
         "port")), reload=args.get("reload"))

2471bde2f2d6125bc1ba36a1a1a56913c8824716
Felipe Alves <felipe.a.araujoo@gmail.com>
2023-03-07 06:49:31 +0000

    method adaptation for multiple audio files

--- maestro_worker_python/utils.py
+++ maestro_worker_python/utils.py
@@ -3,38 +3,45 @@ from __future__ import annotations
 import logging
 import tempfile
 import time
+
 from contextlib import contextmanager
 from urllib.request import urlretrieve
-
+from typing import Dict
 from convert_files import FileToConvert, convert_files
 
 
 @contextmanager
-def prepare_file(url: str, max_duration: float):
-    dst_file = tempfile.NamedTemporaryFile()
-    dst_proc_file = tempfile.NamedTemporaryFile(suffix=".wav")
+def prepare_audios(dict_urls: Dict[str, str], max_duration: float):
+    dst_dict = {}
+    for key in dict_urls.keys():
+        dst_dict[key] = {}
+        dst_dict[key]['dst_file'] = tempfile.NamedTemporaryFile()
+        dst_dict[key]['dst_proc_file'] = tempfile.NamedTemporaryFile(
+            suffix=".wav")
     try:
-        logging.info("Downloading file from %s to %s", url, dst_file.name)
-        urlretrieve(url, dst_file.name)
-        logging.info("Downloaded file to %s", dst_file.name)
-
-        logging.info("Shortening file to %.2f seconds.", max_duration)
-        convert_files(
-            [
+        for key in dst_dict.keys():
+            logging.info(
+                "Downloading file from input -> %s, url -> %s, filename -> %s",
+                key, dict_urls[key], dst_dict[key]['dst_file'].name)
+            urlretrieve(dict_urls[key], dst_dict[key]['dst_file'].name)
+            logging.info("Downloaded file to %s",
+                         dst_dict[key]['dst_file'].name)
+            logging.info("Shortening file to %.2f seconds.", max_duration)
+            convert_files([
                 FileToConvert(
-                    input_file_path=dst_file.name,
-                    output_file_path=dst_proc_file.name,
+                    input_file_path=dst_dict[key]['dst_file'].name,
+                    output_file_path=dst_dict[key]['dst_proc_file'].name,
                     file_format="wav",
                     max_duration=max_duration,
                 )
-            ]
-        )
-        logging.info("Saved shortened file to %s.", dst_proc_file.name)
-
-        yield dst_proc_file.name
+            ])
+            logging.info("Saved shortened file to %s.",
+                         dst_dict[key]['dst_proc_file'].name)
+        yield dst_dict
     finally:
-        dst_file.close()
-        dst_proc_file.close()
+        for key in dst_dict.keys():
+            dst_dict[key]['dst_file'].close()
+            dst_dict[key]['dst_proc_file'].close()
 
 
 class Timer:
